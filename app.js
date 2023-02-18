8861f98eeab9013e8e34ee362abe6172619d291b
oyunbileg-dav <oyunbileg@uni.minerva.edu>
2023-02-18 16:06:44 +0800

    user

--- app.js
+++ app.js
@@ -451,7 +451,7 @@ app.get("/dashboard", auth, (request, response) => {
 });
 
 // get dashboard
-app.get("/get-dashboard", auth, (request, response) => {
+app.get("/get-dashboard", (request, response) => {
   const user = request.user;
   response.json(user)
 });
